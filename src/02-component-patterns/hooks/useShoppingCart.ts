e0da4891e3256d7060eb66a6f9ab78c6b5b605a2
edvalenzuela <edvalenzuelap@gmail.com>
2024-06-24 02:23:34 -0400

    Control props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -12,30 +12,15 @@ export const useShoppingCart = () => {
   const onProductCountChange = ({count, product}:onChangeArgs) => {
     setShoppingCart(old => {
 
-      const productInCart: ProductInCart = old[product.id] || {...product, count: 0}
-      
-      if(Math.max(productInCart.count + count, 0) > 0 ){
-        productInCart.count += count;
-        return{
-          ...old,
-          [product.id]: productInCart
-        }
+      if(count === 0){
+        const { [product.id]:toDelete, ...rest } = old;
+        return rest
       }
 
-      // Borrar el producto
-
-      const { [product.id]:toDelete, ...rest } = old;
-      return rest
-
-      // if(count === 0){
-      //   const { [product.id]:toDelete, ...rest } = old;
-      //   return rest
-      // }
-
-      // return {
-      //   ...old,
-      //   [product.id] : { ...product, count }
-      // }
+      return {
+        ...old,
+        [product.id] : { ...product, count }
+      }
     })
   }
 
